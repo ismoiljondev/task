dcfe12e3ae59488a55a3cd831b588d43cd80c0e7
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-05-10 07:54:18 +0500

    a

--- src/app/utils/withAuth.tsx
+++ src/app/utils/withAuth.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { auth } from "@/firebase";
 
 const withAuth = (WrappedComponent: any) => {
@@ -17,7 +17,7 @@ const withAuth = (WrappedComponent: any) => {
 			return () => {
 				unsubscribe();
 			};
-		}, []);
+		}, [router]);
 
 		return <WrappedComponent {...props} />;
 	};
